4eea960a0c0611ccaa712fe1a8465bc73be881c2
aiokr <hiaiokr@gmail.com>
2020-01-26 15:49:53 +0800

    init hexo-theme-type

---
+++ source/js/app.js
@@ -0,0 +1,31 @@
+var appDaily = {
+  baiduSearch: function (words) {
+    var key = 'site:' + window.location.host + '%20' + words.replace(/\s/g, '%20');
+    var url = 'https://www.baidu.com/baidu?tn=baidu&ie=utf-8&word=';
+
+    window.open(url + key, '_blank');
+  },
+  googleSearch: function (words) {
+    var key = 'site:' + window.location.host + '%20' + words.replace(/\s/g, '%20');
+    var url = 'https://www.google.com/search?q=';
+
+    window.open(url + key, '_blank');
+  },
+  submitSearch: function (search_engines) {
+    var $ipt = document.getElementById('homeSearchInput');
+
+    if (search_engines === 'baidu') {
+      this.baiduSearch($ipt.value.trim());
+    } else {
+      this.googleSearch($ipt.value.trim());
+    }
+
+    return false;
+  },
+};
+
+$(".toc li").replaceWith(function () { return "<li class='mdui-menu-item'>" + this.innerHTML + "</li>"; });
+
+$(".itp-post-toc ol").replaceWith(function () { return "<ul class='mdui-menu mdui-menu-cascade itp-toc-menu' id='toc'>" + this.innerHTML + "</ul>"; });
+
+
