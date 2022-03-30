43928384b5b4155ea02214c7fc71f3a97e92c140
aiokr <hiaiokr@gmail.com>
2022-03-30 01:50:00 +0000

    Auto package by aiokr

--- source/js/app.js
+++ source/js/app.js
@@ -24,7 +24,7 @@ var appDaily = {
   },
 };
 
-//滚动影响appbar颜色
+//滚动影响 toc 透明度
 var whetherChange = 0;
 var whetherChangeToTop = 0;
 var ticking = false;
@@ -33,12 +33,10 @@ cover = 240
 window.onscroll = function () {
   var howFar = document.documentElement.scrollTop || document.body.scrollTop;
   if (howFar > cover & whetherChange == 0) {
-    $("#appbar-index").addClass("itp-appbar-color-pull-up");
     $("#post-toc").addClass("itp-toc-pull-up");
     whetherChange = 1;
   };
   if (howFar <= cover & whetherChange == 1) {
-    $("#appbar-index").removeClass("itp-appbar-color-pull-up");
     $("#post-toc").removeClass("itp-toc-pull-up");
     whetherChange = 0;
   }
