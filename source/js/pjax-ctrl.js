7ee9f100ea7ec5cde5aa6ad8d6cd5bf6e93551b0
aiokr <hiaiokr@gmail.com>
2022-09-10 14:15:20 +0000

    Auto package by aiokr

--- source/js/pjax-ctrl.js
+++ source/js/pjax-ctrl.js
@@ -1,4 +1,4 @@
-/*var pjax = new Pjax({
+var pjax = new Pjax({
   // 在页面进行 PJAX 时需要被替换的元素或容器，一条一个 CSS 选择器，数组形式
   selectors: [
     "title",
@@ -11,5 +11,4 @@
 // 添加重载，其实就是 PJAX 完成之后的操作
 document.addEventListener('pjax:complete', function (){
   // 需要重载的 JS 函数
-  ks.image(":not[no-image] img"); // 重载 Kico Style 的图片灯箱
-});*/
+});
