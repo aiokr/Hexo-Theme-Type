d331a4c9fb21e34e175f0e9b44f7f3c4168c62ab
aiokr <hiaiokr@gmail.com>
2022-11-05 02:44:13 +0800

    ✨ 新增“摄影”、“相册”两种页面

--- source/js/app.js
+++ source/js/app.js
@@ -110,21 +110,4 @@ $(".itp-mag-content img").each(function () {
   $(this).wrap(element);
 })
 
-$('[data-fancybox="gallery"]').fancybox({
-  buttons: [
-    "close",
-  ],
-  baseTpl: '<div class="fancybox-container" role="dialog" tabindex="-1">' +
-    '<div class="fancybox-bg"></div>' +
-    '<div class="fancybox-inner">' +
-    '<div class="fancybox-infobar">' +
-    '<span data-fancybox-index></span>&nbsp;/&nbsp;<span data-fancybox-count></span>' +
-    '</div>' +
-    '<div class="fancybox-toolbar">{{buttons}}</div>' +
-    '<div class="fancybox-navigation">{{arrows}}</div>' +
-    '<div class="fancybox-stage"></div>' +
-    '</div>' +
-    '</div>',
-});
-
 console.log(`${'\n'} %c  Hexo-Theme-Type  %c  https://github.com/aiokr/hexo-theme-type  ${'\n'}`, 'color: #fff; background: #030307; padding:5px 0;', 'color: #fff; background: #71afdd; padding:5px 0;');
